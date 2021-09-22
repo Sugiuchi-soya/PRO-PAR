7765abc8c03238c2ec119988aebcbc5ea0e54e30
Sugiuchi-soya <sugiuchi.soya@gmail.com>
2021-09-22 23:30:34 +0900

    9/22 23:25 アナリティクス導入

--- PRO_PAR/settings.py
+++ PRO_PAR/settings.py
@@ -111,8 +111,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
 
@@ -125,8 +123,7 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
